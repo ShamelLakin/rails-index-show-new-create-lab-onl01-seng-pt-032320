1e62565547883b425f26fdd4c896f8484f3b0e0f
Shamel Lakin <christlife4u@gmail.com>
2020-07-23 16:07:51 -0400

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -2,5 +2,12 @@ class CouponsController < ApplicationController
   def new
     @Coupon = Coupon.all
   end
+
+  def create 
+    @coupon = Coupon.new(params[:coupon_code])
+ 
+    @coupon.save
+    redirect_to @coupon
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'coupon/index'
 
-  resources :coupons 
+  resources :coupons
 
   root 'coupon#index'
 
